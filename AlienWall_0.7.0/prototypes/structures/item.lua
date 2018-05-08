ae9c396396ece6cdc3098c17bd8d4458d7ff8c86
TheBrain0110 <tonofpudding@gmail.com>
2018-05-08 16:41:21 +0200

    Fishycat's unused stuff cleanup & item grouping

--- AlienWall_0.7.0/prototypes/structures/item.lua
+++ AlienWall_0.7.0/prototypes/structures/item.lua
@@ -6,8 +6,8 @@ data:extend(
     icon = "__AlienWall__/graphics/icons/wall/hybrid-wall.png",
 	icon_size = 32,
     flags = {"goes-to-quickbar"},
-    subgroup = "structures",
-    order = "a[hybrid-wall]",
+    subgroup = "defensive-structure",
+    order = "b-c[hybrid-wall]",
     place_result = "hybrid-wall",
     stack_size = 50
   },
@@ -17,8 +17,8 @@ data:extend(
     icon = "__AlienWall__/graphics/icons/gate/hybrid-gate.png",
 	icon_size = 32,
     flags = {"goes-to-quickbar"},
-    subgroup = "structures",
-    order = "b[hybrid-gate]",
+    subgroup = "defensive-structure",
+    order = "b-d[hybrid-gate]",
     place_result = "hybrid-gate",
     stack_size = 50
   },
